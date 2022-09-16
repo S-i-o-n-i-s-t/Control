a6008c7f15a34af33db5d2b8dc996879fdc034f3
S-i-o-n-i-s-t <metalljan@yandex.ru>
2022-09-16 16:40:14 +0300

    пишем программу, которая позволит ввести с терминала строки и заполнит массив

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длинна которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алкогитма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+Console.WriteLine("Введите количество необходимых индексов для массива");
+int c = Convert.ToInt32(Console.ReadLine());
+string[] N = new string[c];
+string[] M = new string[c]; 
+void CreatingArray(string[] Name)
+{
+    for (int index = 0; index < Name.Length;index++)
+    { 
+        Name[index] = (Console.ReadLine());
+    }
+}
+void PrintArray(string[] Name)
+{
+    for (int pozition = 0; pozition < Name.Length; pozition++)
+    {
+        Console.Write($"{Name[pozition]} ");
+    }
+}
+
